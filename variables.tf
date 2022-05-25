bf1c1ff5b3e98da702e7083c489afda137368d32
Anna Szpoton <anna.szpoton@cloudify.co>
2022-05-25 09:41:09 +0200

    Init

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "az_resource_group_name" {
+
+}
+
+variable "cidr_block" {
+
+}
+
+variable "cidr_block_prefix" {
+
+}
+
+
+variable "dns_servers" {
+
+}
+
+
+variable "tags" {
+  default = {}
+}
