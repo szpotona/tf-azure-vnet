92b83ef4f2e94293f7373ecc2b8bf0bc17b921be
Anna Szpoton <anna.szpoton@cloudify.co>
2022-05-30 18:47:29 +0200

    Add type

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "az_resource_group_name" {
-
+  type = string
 }
 
 variable "cidr_block" {
-
+  type = string
 }
 
 variable "cidr_block_prefix" {
-
+  type = string
 }
 
 
 variable "dns_servers" {
-
+  type = list(string)
 }
 
 
