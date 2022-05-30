614ae21b59930f39863bd38737c100113815b4d3
Anna Szpoton <anna.szpoton@cloudify.co>
2022-05-30 14:48:46 +0200

    Fix references

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 data "azurerm_resource_group" "example" {
-  name     = var.rg_name
+  name     = var.az_resource_group_name
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
